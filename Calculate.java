1cabd2e227b89f4eb42110f8ff6f408042a43986
Корзун Максим <korzunm@rias.ru>
2018-07-30 10:33:42 +0700

    lesson 1

---
+++ Calculate.java
@@ -0,0 +1,18 @@
+
+//import java.util.*; 
+
+public class Calculate {
+    public static void main(String[] args) {
+        System.out.println("Hi piple");
+        Integer a, b, c;
+        a = 10; 
+        b = 10;
+        c = a+b;
+        if (a+b == 30) {
+            System.out.println("a+b="+c);
+        } else {
+            System.out.println("a+3*b="+a+3*b);
+        }
+    }
+ 
+}
--- myfirst.java
+++ myfirst.java
@@ -3,7 +3,19 @@ import java.util.*;
 public class myfirst{
     public static void main(String args[]) {
         System.out.println("Hi maksi");
-        
+        int first = Integer.valueOf(args[0]);
+        int second = Integer.valueOf(args[1]);
+        if (first == 1)  
+            System.out.println("first = 1");
+        if (first == 2) 
+            if (second == 2) 
+                System.out.println("first and second = 2");
+        int summ = first + second;
+        System.out.println(first+"+"+second+"="+summ);
+        int i;
+        for(i = 0; i < 10; i++) {
+            System.out.println("i = "+i);
+        }
     }
 }
 
